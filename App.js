a343c29de46eb1ad0dd6c9d843a1cc4222444435
omprakash-singh <omprakashingh11009@gmail.com>
2021-11-24 19:38:54 +0530

    first

---
+++ App.js
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////////
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const display = document.getElementById("display");
+//////////////////////////////////////////////////////////
+one.addEventListener('click', () => {
+    display.innerHTML = "1";
+})
+two.addEventListener('click', () => {
+    display.innerHTML = "2";
+})
